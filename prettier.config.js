9ad1e73000692e74cc15fe30b60e1abb99ab3db8
Ali Houshangi <blumy.info@gmail.com>
2025-05-21 01:02:40 +0330

    prettier config file change

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,15 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 module.exports = {
-	plugins: [require("prettier-plugin-tailwindcss")],
+	plugins: [
+		require("prettier-plugin-tailwindcss"),
+		require("prettier-plugin-organize-imports"),
+		require("prettier-plugin-sort-json"),
+	],
+	tabWidth: 2,
+	semi: false,
+	singleQuote: false,
+	trailingComma: "none",
+	printWidth: 100,
+	bracketSpacing: false,
+	arrowParens: "avoid",
 };
