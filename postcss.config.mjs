17e79a467a173cd7b63804b08e67fab802792f17
Ali Houshangi <blumy.info@gmail.com>
2025-05-21 01:34:32 +0330

    tailwind postcss added

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,7 @@
 const config = {
   plugins: [
     "postcss-import",
+    "@tailwindcss/postcss",
     "tailwindcss",
     "autoprefixer",
     ...(process.env.NODE_ENV === "production" ? ["cssnano"] : []),
