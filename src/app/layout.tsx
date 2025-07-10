403058059e2027b05caf62c31f50662756034d1e
Ali Houshangi <blumy.info@gmail.com>
2025-07-11 01:24:00 +0330

    something update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import { defaultMetadata } from "@/utils/seo";
 import type { Metadata } from "next";
 // PLACEHOLDER_TOASTER_IMPORT
+// PLACEHOLDER_THEME_IMPORT
 
 import { FiraFont, VazirFont } from "@/app/font";
 import RegisterSW from "@/components/layout/RegisterSW";
@@ -27,9 +28,11 @@ export default function RootLayout({
         <RegisterSW />
         <Suspense fallback={null}>
           {/* PLACEHOLDER_REDUX_PROVIDER_START */}
+          {/* PLACEHOLDER_THEME_PROVIDER_START */}
           {/* PLACEHOLDER_I18N_PROVIDER_START */}
           {children}
           {/* PLACEHOLDER_I18N_PROVIDER_END */}
+          {/* PLACEHOLDER_THEME_PROVIDER_END */}
           {/* PLACEHOLDER_REDUX_PROVIDER_END */}
         </Suspense>
         {/* PLACEHOLDER_TOASTER */}
